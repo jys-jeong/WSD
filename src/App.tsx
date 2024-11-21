b207d8a4e450eeb74f6778bca2826dfc8102d45a
jys-jeong <zha1230231@naver.com>
2024-11-21 17:02:35 +0900

    URL 할당

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,16 @@
+// /src/App.tsx
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./components/Auth/AuthContext";
-import ProtectedRoute from "./components/Auth/ProtectedRoute";
-import SignInPage from "./pages/signin";
-import SignUpPage from "./pages/signup";
-import Main from "./pages/home";
+import { BrowserRouter as Router } from "react-router-dom";
+import { AuthProvider } from "./components/Auth/AuthContext"; // AuthProvider 임포트
+import AppRoutes from "./routes"; // 라우팅 설정
 
 const App: React.FC = () => {
   return (
     <AuthProvider>
+      {" "}
+      {/* AuthProvider로 감싸서 하위 컴포넌트들이 인증 상태에 접근 가능 */}
       <Router>
-        <Routes>
-          <Route path="/signin" element={<SignInPage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute>
-                <Main />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
+        <AppRoutes /> {/* 라우팅 설정 */}
       </Router>
     </AuthProvider>
   );
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import MovieList from "../components/home/MovieList";
 import Banner from "../components/home/Banner";
-
+import Header from "../components/Header";
 const Main: React.FC = () => {
   return (
     <div>
+      <Header />
       <Banner />
       <MovieList category="now_playing" title="지금 뜨는 콘텐츠" />
       <MovieList category="top_rated" title="최고 평점 콘텐츠" />

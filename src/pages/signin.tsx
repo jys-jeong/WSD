cf247c295769e54248339a986c88faf4c2e36b89
jys-jeong <zha1230231@naver.com>
2024-11-26 17:48:38 +0900

    signIn 링크 페이지 디자인 수정

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,17 +1,16 @@
-// src/pages/AuthPage.tsx
 import React, { useState } from "react";
 import SignIn from "../components/Auth/SignIn";
 import SignUp from "../components/Auth/Signup";
 import "../assets/styles/AuthForm.css";
 
 const AuthPage: React.FC = () => {
-  const [isSignIn, setIsSignIn] = useState(true);
-  const [isAnimating, setIsAnimating] = useState(false);
+  const [isSignIn, setIsSignIn] = useState(true); // 현재 상태 (SignIn 또는 SignUp)
+  const [isAnimating, setIsAnimating] = useState(false); // 애니메이션 상태
 
   const handleToggle = () => {
     setIsAnimating(true); // 애니메이션 시작
     setTimeout(() => {
-      setIsSignIn(!isSignIn); // 상태 전환
+      setIsSignIn((prev) => !prev); // 상태 전환 (SignIn -> SignUp, 또는 그 반대)
       setIsAnimating(false); // 애니메이션 종료
     }, 500); // 애니메이션 지속 시간 (0.5초)
   };

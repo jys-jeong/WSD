9a99ed2483dfcbf11de18139101ece361a5a29ec
jys-jeong <zha1230231@naver.com>
2024-11-27 03:18:37 +0900

    Routing 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-// /src/App.tsx
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { AuthProvider } from "./components/Auth/AuthContext"; // AuthProvider 임포트
@@ -7,8 +6,9 @@ import AppRoutes from "./routes"; // 라우팅 설정
 const App: React.FC = () => {
   return (
     <AuthProvider>
-      {" "}
-      <Router>
+      <Router basename="/WSD">
+        {" "}
+        {/* 기본 경로를 WSD로 설정 */}
         <AppRoutes /> {/* 라우팅 설정 */}
       </Router>
     </AuthProvider>

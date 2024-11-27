0dd3aefb5a19827f5a2c72f793ed58e8a54c97a8
jys-jeong <zha1230231@naver.com>
2024-11-27 11:42:40 +0900

    header 모바일 디자인

--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -2,8 +2,16 @@ import React from "react";
 import useFetchBannerMovie from "../../hooks/useFetchBannerMovie";
 import "../../assets/styles/Banner.css";
 
+const truncate = (text: string | undefined, maxLength: number): string => {
+  if (!text) return "";
+  return text.length > maxLength
+    ? text.substring(0, maxLength - 3) + "..."
+    : text;
+};
+
 const Banner: React.FC = () => {
   const { movie, loading, error } = useFetchBannerMovie();
+  const isMobile = window.matchMedia("(max-width: 400px)").matches; // 400px 이하 여부 확인
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>{error}</p>;
@@ -19,7 +27,10 @@ const Banner: React.FC = () => {
     >
       <div className="banner_contents">
         <h1 className="banner_title">{movie?.title}</h1>
-        <p className="banner_description">{movie?.overview}</p>
+        <p className="banner_description">
+          {isMobile ? truncate(movie?.overview, 50) : movie?.overview}{" "}
+          {/* 모바일에서만 truncate */}
+        </p>
       </div>
       <div className="banner--fadeBottom" />
     </div>
